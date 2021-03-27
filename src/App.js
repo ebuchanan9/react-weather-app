251e4ea40f127383bd38142e6322b6c815708bc2
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-03-27 11:30:55 -0400

    added source

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import Units from "./Units";
 export default function App() {
   return (
     <div className="App">
+      <div className = "Weather">
       <Search />
       <Overview />
       <Units />
+    </div>
+    <p>
+      <a href = "https://github.com/ebuchanan9/react-weather-app">Open-Source Code</a> by <a href = "https://www.linkedin.com/in/erin-buchanan-lmsw-ba2a2b160/">Erin Buchanan</a>
+    </p>
     </div>
   );
 }
