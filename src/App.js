f48cf2921590159bbc3aef83e528814d7c9b8905
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-03-27 14:16:59 -0400

    edited footer and max width

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import Units from "./Units";
 export default function App() {
   return (
     <div className="App">
+      <div className = "container">
       <Search />
       <Overview />
       <Units />
+          <footer>
+        <p>
+          <a href="https://github.com/ebuchanan9/react-weather-app">Open-Source Code</a> by <a
+            href="https://www.linkedin.com/in/erin-buchanan-lmsw-ba2a2b160/">Erin Buchanan</a>
+        </p>
+      </footer>
+    </div>
     </div>
   );
 }
